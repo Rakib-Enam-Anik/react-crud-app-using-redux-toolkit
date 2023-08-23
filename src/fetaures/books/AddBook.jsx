943cf34de617535fab83b9993b002fae94f688f0
Rakib Enam <md.rakib.enam.anik@bracu.ac.bd>
2023-08-23 16:08:00 +0600

    CRUD file added

--- src/fetaures/books/AddBook.jsx
+++ src/fetaures/books/AddBook.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 const AddBook = () => {
+  const [title, setTitle] = useState("")
   return (
     <div>
       <h2>Add Book</h2>
+      <form>
+        <div className="form-field">
+          <label htmlFor="title">Title: </label>
+          <input type="text" id="title" name="title" value={title} onChange={
+            (e) => setTitle(e.target.value)
+            } required />
+        </div>
+      </form>
     </div>
+
+    
   );
 };
 
